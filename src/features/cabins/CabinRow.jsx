ef3a2e2f3596bfe26cb6b9bde5c219d8e1f1a7f6
Adelana107 <adelana787898@gmail.com>
2025-11-20 19:29:24 +0100

    Fetching Cabin Data

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,6 @@
+import PropTypes from "prop-types";
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
 
 const TableRow = styled.div`
   display: grid;
@@ -12,29 +14,62 @@ const TableRow = styled.div`
   }
 `;
 
+const CabinInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
 const Img = styled.img`
-  display: block;
   width: 6.4rem;
   aspect-ratio: 3 / 2;
   object-fit: cover;
   object-position: center;
-  transform: scale(1.5) translateX(-7px);
+  border-radius: 4px;
 `;
 
-const Cabin = styled.div`
+const CabinName = styled.div`
   font-size: 1.6rem;
   font-weight: 600;
   color: var(--color-grey-600);
-  font-family: "Sono";
 `;
 
 const Price = styled.div`
-  font-family: "Sono";
   font-weight: 600;
 `;
 
 const Discount = styled.div`
-  font-family: "Sono";
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+function CabinRow({ cabin, index }) {
+  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+
+  return (
+    <TableRow role="row">
+      <div>{index + 1}</div>
+      <CabinInfo>
+        <Img src={image} alt={name} />
+        <CabinName>{name}</CabinName>
+      </CabinInfo>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{discount ? `${discount}%` : "-"}</Discount>
+      <div>Delete</div>
+    </TableRow>
+  );
+}
+
+CabinRow.propTypes = {
+  cabin: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    maxCapacity: PropTypes.number.isRequired,
+    regularPrice: PropTypes.number.isRequired,
+    discount: PropTypes.number,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+export default CabinRow;
